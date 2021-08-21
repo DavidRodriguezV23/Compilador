564ae12c28c406a7cf478bc8e4af822f6bbf3a86
David Armando Rodríguez Varón <darvaront@gmail.com>
2021-08-21 15:31:25 -0500

    Modificaciones menores

---
+++ README.md
@@ -0,0 +1,1 @@
+Compilador - David Armando Rodríguez Varón - 20181020041 - Juan Sebastian Sánchez Tabares - 20181020008
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from anlex.analizador_lexico import *
 
+'''
+    COMPILADOR REALIZADO POR:
+    DAVID ARMANDO RODRÍGUEZ VARÓN - 20181020041
+    JUAN SEBASTIÁN SÁNCHEZ TABARES - 20181020008
+'''
+
 def main():
     lines = load_file("Programa.cpp")
     #lines = ["18+22=40;"]
